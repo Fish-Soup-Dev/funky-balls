a5bfa92fc2b688513ff9b12f4e1473643e59771f
soup <64437861+Fish-game-dev@users.noreply.github.com>
2021-10-10 14:14:04 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import math
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+RED = (230, 70, 30)
 
 ScreenY = 720
 ScreenX = 1280
 
-Ball_S = 70
-Friction = 0.0
+Ball_S = 10
+Friction = 0.00
 
 class Ball:
     def __init__(self):
@@ -36,8 +37,10 @@ def make_ball():
 
 def move(c, v, r, m):
     c += v
-    if c < r: c, v = r, -v
-    if c > m-r: c, v = m-r, -v   
+    if c < r:
+        c, v = r, -v
+    if c > m-r:
+        c, v = m - r, -v   
     return c, v
 
 def main():
@@ -48,7 +51,7 @@ def main():
 
     ball_list = []
 
-    for x in range(3):
+    for x in range(30):
         ball = make_ball()
         ball_list.append(ball)
 
@@ -71,12 +74,40 @@ def main():
                 if a != ball:
                     v1 = pygame.math.Vector2(ball.x, ball.y)
                     v2 = pygame.math.Vector2(a.x, a.y)
+
                     if v1.distance_to(v2) < Ball_S + Ball_S:
+                        ball.color = RED
+                        a.color = RED
+
                         nv = v2 - v1
+
                         m1 = pygame.math.Vector2(ball.change_x, ball.change_y).reflect(nv)
                         m2 = pygame.math.Vector2(a.change_x, a.change_y).reflect(nv)
+
+                        if m1.y < 0:
+                            m1.y += Friction * 10
+                        if m1.y > 0:
+                            m1.y -= Friction * 10
+
+                        if m1.x < 0:
+                            m1.x += Friction * 10
+                        if m1.x > 0:
+                            m1.x -= Friction * 10
+                        if m2.y < 0:
+                            m2.y += Friction * 10
+                        if m2.y > 0:
+                            m2.y -= Friction * 10
+
+                        if m2.x < 0:
+                            m2.x += Friction * 10
+                        if m2.x > 0:
+                            m2.x -= Friction * 10
+
                         ball.change_x, ball.change_y = m1.x, m1.y
                         a.change_x, a.change_y = m2.x, m2.y
+                    
+                    else:
+                        ball.color = WHITE
 
             if ball.change_y < 0:
                 ball.change_y += Friction
